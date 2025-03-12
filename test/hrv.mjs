8ffec13d2a2c7f41f183a26c4d94b8d2cbbbc6a7
Carlos Saito <carlos.saito@optimizely.com>
2025-03-12 15:45:27 +0100

    Content area types

--- test/hrv.mjs
+++ test/hrv.mjs
@@ -13,6 +13,34 @@ export default buildConfig({
         summary: {
           type: "string",
         },
+        sections: {
+          type: "content",
+          allowedTypes: ["Section", "Section3"],
+        },
+      },
+    },
+    Section: {
+      baseType: "component",
+      properties: {
+        heading: {
+          type: "string",
+        },
+      },
+    },
+    Section2: {
+      baseType: "component",
+      properties: {
+        heading: {
+          type: "string",
+        },
+      },
+    },
+    Section3: {
+      baseType: "component",
+      properties: {
+        heading: {
+          type: "string",
+        },
       },
     },
   },
